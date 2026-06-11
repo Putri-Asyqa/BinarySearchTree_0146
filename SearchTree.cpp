d7e6338239ef64ab68edb669519e185a8c6b2de3
Putri Asyqa <putriasyqa25lindra@gmail.com>
2026-06-11 10:52:48 +0700

    bikin class binarytree

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -11,5 +11,12 @@ public:
     Node *leftchild;
     Node *rightchild;
 
-    Node(string i, Node *1, Node *r)
-}
+    Node(string i, Node *l, Node *r)
+    {
+        info = i;
+        leftchild = l;
+        rightchild = r;
+    }
+};
+
+class BinaryTree
